1a7f1c591637691d4cf8d1c2e65453917fbea14c
FelipeBonfante <felipe.satto@hotmail.com>
2017-06-01 23:18:15 -0300

    removendo arquivos não utilizados

--- trainingAndNeuralNetworkTools/testernn.m
+++
@@ -1,9 +0,0 @@
-clc,clear all,close all
-
-[X,T] = simpleseries_dataset;
-net = layrecnet(1:2,10);
-[Xs,Xi,Ai,Ts] = preparets(net,X,T);
-net = train(net,Xs,Ts,Xi,Ai);
-view(net)
-Y = net(Xs,Xi,Ai);
-perf = perform(net,Y,Ts)
