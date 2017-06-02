c8067f8ade940dbc97b9a0036506cdce43047e92
FelipeBonfante <felipe.satto@hotmail.com>
2017-06-02 14:41:03 -0300

    removendo mais comentários e linhas desnecessárias. Adicionando alguns comentários em trechos que não estavam claros

--- mainroutine.m
+++ mainroutine.m
@@ -39,7 +39,7 @@ vogaisref = cell(1,size(yframed,1));
 
 % carrega a rede neural treinada para usar na classificação
 load nettr22;
-
+view(nettr)
 % variáveis para controle de índices de acesso
 i = 1;
 p = 2;
@@ -57,77 +57,79 @@ totalframes = size(yframed,1);
 % variável pra armazenar a quantidade de \as\ encontrados
 contadordeas = 0;
 % começa a iteração nos frames para análise e classificação
-while i<totalframes
-    % extrai o frame atual para análise
-    frame = y(i*framelength:p*framelength);
-    
-    % extrai o mfcc do trecho
-    ceps = extractmfcc(frame,Fs);
-    
-    % aplica os coeficientes na rede para obter a saída estimada
-    saida = nettr(ceps');
-    saidaa = max(saida);
-    
-    
-    % verifica se a saída representa um possível a    
-    if saidaa >=0.9
-        disp('possível a encontrado')
-        % agora começa uma sub verificação, para ver até onde o possível 
-        % a se extende        
-        curr = i + 1;
-        pos = curr + 1;
-        % flag para indicar se é ou não a
-        flag = 1;
-        % enquanto a flag for positiva e não se atingiu o final do áudio
-        while(flag && curr<totalframes)
-            % repete os mesmos processos anteriores para classificar
-            frameaux = y(curr*framelength:pos*framelength);
-            cepsaux = extractmfcc(frameaux,Fs);
-            saidaaux = nettr(cepsaux');            
-            saidaauxa = max(saidaaux);    
-            if saidaauxa >= 0.9
-                flag = 1;
-                curr = curr + 1;
-                pos = curr+1;
-            else
-                flag = 0;
-                flag2 =1;
-                acumulador = [];
-                k = i;
-                while k < curr
-                    low = k;
-                    high = k+1;
-                    acumular = y(low*framelength:high*framelength-1);
-                    acumulador = cat(1,acumulador,acumular);                    
-                    k = k + 1;
-                end
-                i = curr - 1;
-            end
-        end
-        
-        % se encontrou vogal \a\ a flag 2 estará true
-        if flag2
-            % se o trecho de possível a for significante, isto é, ter pelo 
-            % menos tamanho superior a 40ms, este é contabilizado e salvo
-            if length(acumulador)>2*framelength+1
-                disp('encontrei um a')
-                contadordeas = contadordeas + 1;
-                saveaudiofile(acumulador,Fs);
-            end
-            % zera a flag para a análise do possível próximo a
-            flag2 = 0;
-        end
-       
-        
-    end
-    i = i + 1;
-    p = i + 1;
-
-end
-
-disp('fim do procedimento')
-contadorstr = num2str(contadordeas);
-saidatexto = sprintf('foram encontrados %s fonemas a',contadorstr);
-disp(saidatexto)
+% while i<totalframes
+%     % extrai o frame atual para análise
+%     frame = y(i*framelength:p*framelength);
+%     
+%     % extrai o mfcc do trecho
+%     ceps = extractmfcc(frame,Fs);
+%     
+%     % aplica os coeficientes na rede para obter a saída estimada
+%     saida = nettr(ceps');
+%     saidaa = max(saida);
+%     
+%     
+%     % verifica se a saída representa um possível a    
+%     if saidaa >=0.9
+%         disp('possível a encontrado')
+%         % agora começa uma sub verificação, para ver até onde o possível 
+%         % a se extende        
+%         curr = i + 1;
+%         pos = curr + 1;
+%         % flag para indicar se é ou não a
+%         flag = 1;
+%         % enquanto a flag for positiva e não se atingiu o final do áudio
+%         while(flag && curr<totalframes)
+%             % repete os mesmos processos anteriores para classificar
+%             frameaux = y(curr*framelength:pos*framelength);
+%             cepsaux = extractmfcc(frameaux,Fs);
+%             saidaaux = nettr(cepsaux');            
+%             saidaauxa = max(saidaaux); 
+%             % se ainda é a continua a rotina incrementando as variáveis de
+%             % indice
+%             if saidaauxa >= 0.9
+%                 flag = 1;
+%                 curr = curr + 1;
+%                 pos = curr+1;
+%             else
+%                 flag = 0;
+%                 flag2 =1;
+%                 acumulador = [];
+%                 k = i;
+%                 while k < curr
+%                     low = k;
+%                     high = k+1;
+%                     acumular = y(low*framelength:high*framelength-1);
+%                     acumulador = cat(1,acumulador,acumular);                    
+%                     k = k + 1;
+%                 end
+%                 i = curr - 1;
+%             end
+%         end
+%         
+%         % se encontrou vogal \a\ a flag 2 estará true
+%         if flag2
+%             % se o trecho de possível a for significante, isto é, ter pelo 
+%             % menos tamanho superior a 40ms, este é contabilizado e salvo
+%             if length(acumulador)>2*framelength+1
+%                 disp('encontrei um a')
+%                 contadordeas = contadordeas + 1;
+%                 saveaudiofile(acumulador,Fs);
+%             end
+%             % zera a flag para a análise do possível próximo a
+%             flag2 = 0;
+%         end
+%        
+%         
+%     end
+%     i = i + 1;
+%     p = i + 1;
+% 
+% end
+% 
+% disp('fim do procedimento')
+% contadorstr = num2str(contadordeas);
+% saidatexto = sprintf('foram encontrados %s fonemas a',contadorstr);
+% disp(saidatexto)
 
 
--- training/training.m
+++ training/training.m
@@ -1,53 +1,52 @@
-% clc
-% clear all
-% close all
+clc
+clear all
+close all
+
+% add as pastas util e voicebox no path para utilizar as funções auxiliares
+addpath(genpath('..\util\'));
+addpath(genpath('..\voicebox'));
+
+% add a pasta dos áudios ao path
+addpath(genpath('vogais\'));
+
+%recupera as listas de arquivos
+afilelist = getAllFiles('vogais\a','*.wav',0);
+efilelist = getAllFiles('vogais\e','*.wav',0);
+ehfilelist = getAllFiles('vogais\eh','*.wav',0);
+ifilelist = getAllFiles('vogais\i','*.wav',0);
+ofilelist = getAllFiles('vogais\o','*.wav',0);
+ohfilelist = getAllFiles('vogais\oh','*.wav',0);
+ufilelist = getAllFiles('vogais\u','*.wav',0);
+
+%obtém-se as matrizes de mfcc de todos eles
+amfcc = getMFCCDataMatrix(afilelist);
+emfcc = getMFCCDataMatrix(efilelist);
+ehmfcc = getMFCCDataMatrix(ehfilelist);
+imfcc = getMFCCDataMatrix(ifilelist);
+omfcc = getMFCCDataMatrix(ofilelist);
+ohmfcc = getMFCCDataMatrix(ohfilelist);
+umfcc = getMFCCDataMatrix(ufilelist);
+
+amfccmatrix = cell2mat(amfcc(1:end-7));
+emfccmatrix = cell2mat(emfcc(1:end));
+ehmfccmatrix = cell2mat(ehmfcc(1:end));
+imfccmatrix = cell2mat(imfcc(1:end));
+omfccmatrix = cell2mat(omfcc(1:end));
+ohmfccmatrix = cell2mat(ohmfcc(1:end));
+umfccmatrix = cell2mat(umfcc(1:end));
+
+
+%treinando rna
+
+%cria o vetor de treino e o targets
+totaldata=[amfccmatrix;emfccmatrix;ehmfccmatrix;imfccmatrix;omfccmatrix;ohmfccmatrix;umfccmatrix];
 % 
-% % add as pastas util e voicebox no path para utilizar as funções auxiliares
-% addpath(genpath('..\util\'));
-% addpath(genpath('..\voicebox'));
 % 
-% % add a pasta dos áudios ao path
-% addpath(genpath('vogais\'));
-% 
-% %recupera as listas de arquivos
-% afilelist = getAllFiles('vogais\a','*.wav',0);
-% efilelist = getAllFiles('vogais\e','*.wav',0);
-% ehfilelist = getAllFiles('vogais\eh','*.wav',0);
-% ifilelist = getAllFiles('vogais\i','*.wav',0);
-% ofilelist = getAllFiles('vogais\o','*.wav',0);
-% ohfilelist = getAllFiles('vogais\oh','*.wav',0);
-% ufilelist = getAllFiles('vogais\u','*.wav',0);
-% 
-% %obtém-se as matrizes de mfcc de todos eles
-% amfcc = getMFCCDataMatrix(afilelist);
-% emfcc = getMFCCDataMatrix(efilelist);
-% ehmfcc = getMFCCDataMatrix(ehfilelist);
-% imfcc = getMFCCDataMatrix(ifilelist);
-% omfcc = getMFCCDataMatrix(ofilelist);
-% ohmfcc = getMFCCDataMatrix(ohfilelist);
-% umfcc = getMFCCDataMatrix(ufilelist);
-% 
-% amfccmatrix = cell2mat(amfcc(1:end-7));
-% emfccmatrix = cell2mat(emfcc(1:end));
-% ehmfccmatrix = cell2mat(ehmfcc(1:end));
-% imfccmatrix = cell2mat(imfcc(1:end));
-% omfccmatrix = cell2mat(omfcc(1:end));
-% ohmfccmatrix = cell2mat(ohmfcc(1:end));
-% umfccmatrix = cell2mat(umfcc(1:end));
-% 
-% 
-% %treinando rna
-% 
-% %cria o vetor de treino e o targets
-% totaldata=[amfccmatrix;emfccmatrix;ehmfccmatrix;imfccmatrix;omfccmatrix;ohmfccmatrix;umfccmatrix];
-% 
-% 
-% targets = zeros(1,size(totaldata,1));
-% targets(1:size(amfccmatrix,1)) = 1;
+targets = zeros(1,size(totaldata,1));
+targets(1:size(amfccmatrix,1)) = 1;
 % %targets = vertcat(targetsa,targetse,targetseh,targetsi,targetso,targetsoh,targetsu);
 % 
-% totaldata = totaldata';
-% totaldata = mapminmax(totaldata);
+totaldata = totaldata';
 % 
 % gpu1 = gpuDevice(1)
 % net = patternnet([150 100 50]);
@@ -57,27 +56,29 @@
 % net.divideParam.valRatio = 15/100;
 % net.divideParam.testRatio = 15/100;
 % 
-% Q = size(totaldata,2);
-% Q1 = floor(Q * 0.90);
-% Q2 = Q - Q1;
-% ind = randperm(Q);
-% ind1 = ind(1:Q1);
-% ind2 = ind(Q1 + (1:Q2));
-% x1 = totaldata(:, ind1);
-% t1 = targets(:, ind1);
-% x2 = totaldata(:, ind2);
-% t2 = targets(:, ind2);
+% gera as matrizes de treinamento e validação
+Q = size(totaldata,2);
+Q1 = floor(Q * 0.80);
+Q2 = Q - Q1;
+ind = randperm(Q);
+ind1 = ind(1:Q1);
+ind2 = ind(Q1 + (1:Q2));
+x1 = totaldata(:, ind1);
+t1 = targets(:, ind1);
+x2 = totaldata(:, ind2);
+t2 = targets(:, ind2);
+
+load nettr22.mat;
+teste = nettr(x1);
+teste2 = nettr(x2);
+plotconfusion(t1,teste,'treinamento',t2,teste2,'validação');
 % 
 % numNN = 10;
+% % célula para armazenar as redes treinadas
 % NN = cell(1, numNN);
+% % células pra armazenar os desempenhos das redes 
 % perfs = zeros(1, numNN);
 % perfs2 = zeros(1,numNN);
-% 
-% 
-% % svmmodel = fitcsvm(x1',t1','KernelFunction','gaussian','Standardize',true,'ClassNames',[-1,1]);
-% % svmmodelval = crossval(svmmodel)
-% % svmmodelval.Trained{1}
-% % error = kfoldLoss(svmmodelval)
 % 
 % %net2 = configure(net,totaldata,targets);
 % for i = 1:numNN
@@ -90,4 +91,4 @@
 
 
 % nettr = NN{2};
-% save ('nettr22.mat','nettr');
+% save ('nettr23.mat','nettr');
